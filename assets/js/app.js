2584e1c315862109b129f33c480b8b64a21e6a89
edelBCS <edelm8@gmail.com>
2019-08-04 19:46:17 -0700

    added chat box

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,15 +8,18 @@ var playerChoice = "";
 var opponentChoice = "";
 var playerStatus = "";
 var opponentStatus = "";
+var wins = 0;
+var losses = 0;
+var currentPlayer = "";
 
-
-var currentPlayer = prompt("Name");
-sessionStorage.setItem("Name", currentPlayer);
 $("#newGameBtnDiv").hide();
 
+getPlayerName();
 
-//$(document).ready(function () {
-// Your web app's Firebase configuration
+
+
+
+//Firebase configuration
 firebaseConfig = {
     apiKey: "AIzaSyACzAWnyG_pLE7-4l0939rWUvevQMC2ZHc",
     authDomain: "rpsgame-4a7a0.firebaseapp.com",
@@ -39,7 +42,6 @@ connectionsRef = database.ref("/connections");
 // the client's connection state changes.
 // '.info/connected' is a boolean value, true if the client is connected and false if they are not.
 connectedRef = database.ref(".info/connected");
-//});
 
 // When the client's connection state changes...
 connectedRef.on("value", function (snap) {
@@ -55,33 +57,51 @@ connectedRef.on("value", function (snap) {
     }
 });
 
+function getPlayerName() {
+
+    currentPlayer = prompt("Name");
+    sessionStorage.setItem("Name", currentPlayer);
+}
+
 // When first loaded or when the connections list changes...
 connectionsRef.on("value", function (snapshot) {
-
-    //checks if there are no more than 2 players
-    if (snapshot.numChildren() === 1) {
-        var newUsersRef = database.ref().child("players").child(currentPlayer);
-        var setup = {
-            choice: "",
-            status: "waiting"
-        };
-        newUsersRef.set(setup);
-    } else if (snapshot.numChildren() === 2) {
-        var newUsersRef = database.ref().child("players").child(currentPlayer);
-        var setup = {
-            choice: "",
-            status: "playing"
-        };
-        newUsersRef.set(setup);
-    } else {
-        console.log("No more player slots available!")
-    }
+    // if (snapshot.numChildren() > 2) {
+    //     $("#newGameBtnDiv").hide();
+    //     $("#buttonsDiv").hide();
+    //     $("#fightInfo").text(`No player slot available...Sorry`)
+    // } else {
+        //checks if there are no more than 2 players
+        if (snapshot.numChildren() === 1) {
+            var newUsersRef = database.ref().child("players").child(currentPlayer);
+            var setup = {
+                choice: "",
+                status: "waiting",
+                wins: 0,
+                losses: 0
+            };
+            newUsersRef.set(setup);
+            $("#playerScore").html(`${currentPlayer}<br><br>Wins: ${wins}<br>Losses: ${losses}`);
+            $("#opponentScore").html(`Waiting for Opponent...`);
+        } else if (snapshot.numChildren() === 2) {
+            var newUsersRef = database.ref().child("players").child(currentPlayer);
+            var setup = {
+                choice: "",
+                status: "playing",
+                wins: 0,
+                losses: 0
+            };
+            newUsersRef.set(setup);
+            $("#playerScore").html(`${currentPlayer}<br><br>Wins: ${wins}<br>Losses: ${losses}`);
+            $("#fightInfo").text(`Choose Your Weapon`);
+            $("#opponentScore").html(`Opponent<br><br>Wins: ${wins}<br>Losses: ${losses}`);
+        }
+    // }
 
     //removes player from database on disconnect
     (sessionStorage.getItem("Name") === currentPlayer) ? database.ref("/players/" + currentPlayer).onDisconnect().remove(): "";
 
     //Displays number of players connected
-    $("#chat").text(snapshot.numChildren());
+    //$("#chat").text(snapshot.numChildren());
 });
 
 //when player selects their choice it updates the db
@@ -104,13 +124,16 @@ database.ref("/players/").on("child_changed", function (snap) {
 
         if (playerChoice != "" && opponentChoice != "") {
             drawPlayerSelection("opponentDiv", opponentChoice);
-            console.log(whoWon());
+
             database.ref("/players/" + currentPlayer).update({
                 status: "finished"
             });
+
+            console.log(whoWon());
+
             opponentStatus = "finished";
             playerStatus = "finished";
-            
+
         }
     }
 
@@ -122,14 +145,24 @@ database.ref("/players/").on("child_changed", function (snap) {
             $("#fightInfo").text("Opponent is Ready");
             opponentStatus = snap.val().status;
         }
-            
-        if(opponentStatus === "ready" && playerStatus === "ready"){
+
+        if (opponentStatus === "ready" && playerStatus === "ready") {
             resetGame();
             $("#fightInfo").text("Select Your Weapon!");
             database.ref("/players/" + currentPlayer).update({
                 status: "playing"
             });
         }
+    }
+
+    if (snap.val().status === "finished") {
+        if (snap.key === currentPlayer) {
+            $("#playerScore").html(`${currentPlayer}<br><br>Wins: ${snap.val().wins}<br>Losses: ${snap.val().losses}`)
+        } else {
+            $("#opponentScore").html(`Opponent<br><br>Wins: ${snap.val().wins}<br>Losses: ${snap.val().losses}`)
+        }
+
+
     }
 });
 
@@ -147,8 +180,6 @@ function resetGame() {
     opponentChoice = "";
     $("#playerDiv").empty();
     $("#opponentDiv").empty();
-    
-
 }
 
 function whoWon() {
@@ -159,36 +190,31 @@ function whoWon() {
 
     if (playerChoice === opponentChoice) {
         fightText = `It's a Tie: ${playerChoice} vs ${opponentChoice}`;
-    } else if (playerChoice === 'rock' && (opponentChoice === ('paper' || 'spock'))) {
-        if (opponentChoice === 'paper')
-            fightNoun = "covers";
-        if (opponentChoice === 'spock')
-            fightNoun = "vaporizes";
+    } else if (playerChoice === "rock" && (opponentChoice === "paper" || opponentChoice === "spock")) {
+        (opponentChoice === "paper") ? fightNoun = "covers": "";
+        (opponentChoice === "spock") ? fightNoun = "vaporizes": "";
         fightText = `You Lose: ${opponentChoice} ${fightNoun} ${playerChoice}`;
-    } else if (playerChoice === 'paper' && (opponentChoice === ('scissors' || 'lizard'))) {
-        if (opponentChoice === 'scissors')
-            fightNoun = "cut";
-        if (opponentChoice === 'lizard')
-            fightNoun = "eats";
+        ++losses;
+    } else if (playerChoice === "paper" && (opponentChoice === "scissors" || opponentChoice === "lizard")) {
+        (opponentChoice === "scissors") ? fightNoun = "cut": "";
+        (opponentChoice === "lizard") ? fightNoun = "eats": "";
         fightText = `You Lose: ${opponentChoice} ${fightNoun} ${playerChoice}`;
-    } else if (playerChoice === 'scissors' && (opponentChoice === ('rock' || 'spock'))) {
-        if (opponentChoice === 'rock')
-            fightNoun = "crushes";
-        if (opponentChoice === 'spock')
-            fightNoun = "smashes";
+        ++losses;
+    } else if (playerChoice === "scissors" && (opponentChoice === "rock" || opponentChoice === "spock")) {
+        (opponentChoice === "rock") ? fightNoun = "crushes": "";
+        (opponentChoice === "spock") ? fightNoun = "smashes": "";
         fightText = `You Lose: ${opponentChoice} ${fightNoun} ${playerChoice}`;
-    } else if (playerChoice === 'spock' && (opponentChoice === ('paper' || 'lizard'))) {
-        if (opponentChoice === 'paper')
-            fightNoun = "disproves";
-        if (opponentChoice === 'lizard')
-            fightNoun = "poisons";
+        ++losses;
+    } else if (playerChoice === "spock" && (opponentChoice === "paper" || opponentChoice === "lizard")) {
+        (opponentChoice === "paper") ? fightNoun = "disproves": "";
+        (opponentChoice === "lizard") ? fightNoun = "poisons": "";
         fightText = `You Lose: ${opponentChoice} ${fightNoun} ${playerChoice}`;
-    } else if (playerChoice === 'lizard' && (opponentChoice === ('scissors' || 'rock'))) {
-        if (opponentChoice === 'scissors')
-            fightNoun = "decapitates";
-        if (opponentChoice === 'rock')
-            fightNoun = "crushes";
+        ++losses;
+    } else if (playerChoice === "lizard" && (opponentChoice === "scissors" || opponentChoice === "rock")) {
+        (opponentChoice === "scissors") ? fightNoun = "decapitates": "";
+        (opponentChoice === "rock") ? fightNoun = "crushes": "";
         fightText = `You Lose: ${opponentChoice} ${fightNoun} ${playerChoice}`;
+        ++losses;
     } else {
         if (playerChoice === "rock") {
             if (opponentChoice === "scissors")
@@ -217,7 +243,16 @@ function whoWon() {
                 fightNoun = "vaporizes";
         }
         fightText = `Winner: ${playerChoice} ${fightNoun} ${opponentChoice}`;
+        ++wins;
     }
+
+    console.log(`Wins: ${wins}, Losses: ${losses}`);
+
+    var temp = {
+        wins: wins,
+        losses: losses
+    }
+    database.ref("/players/" + currentPlayer).update(temp);
 
     $("#fightInfo").text(fightText);
     //database.ref("/players/" + currentPlayer).update({status: "finished"});
@@ -262,5 +297,3 @@ function drawPlayerSelection(playerDiv, selection) {
 
     $(`#${playerDiv}`).append(spriteDiv).append(spriteText);
 }
-
-//NEED TO FIX IS PLAYERS HAVE THE SAME NAME
